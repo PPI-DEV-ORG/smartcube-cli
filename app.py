b351b63925df28ed943c5ad5862a8f805dc0284b
IyanTRICKSTER <akiyan2002@gmail.com>
2023-09-07 11:34:05 +0700

    add type hint in detectObject lambda function

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
-import cv2
 import multiprocessing
 from internal.modules.IPCamera import *
 from internal.modules.USBCamera import *
 from internal.modules.VideoProcessor import *
 from internal.modules.ObjectDetectorModel import *
-
-
-def present(frame, bbox):
-    cv2.rectangle(frame, bbox[0], bbox[1], (0, 255, 0), 2)
-    cv2.imshow(f'Camera [x]', frame)
-
 
 def main():
 
@@ -20,17 +13,17 @@ def main():
     videoProcessor = VideoProcessor()
 
     #Init Cameras
-    cameras = [
+    cameras: list[CameraDevice] = [
         IPCamera("rtsp://192.168.212.254:8080/h264.sdp", {'device_vendor_number': 'MK100212'}, videoProcessor),
         USBCamera(0, {'device_vendor_number': 'MLL100212'}, videoProcessor)
     ]
 
     # Mulai proses pemrosesan untuk setiap kamera
+    detectObject: Callable[[np.ndarray], None] = lambda frame: objectDetectorModel.processFrame(frame, videoProcessor.redraw)
+
     processes = []
     for camera in cameras:
-        p = multiprocessing.Process(
-            target=camera.streamVideoFrame, 
-            args=((lambda frame: objectDetectorModel.processFrame(frame, present)),))
+        p = multiprocessing.Process(target=camera.streamVideoFrame, args=(detectObject,))
         p.start()
         processes.append(p)
 
