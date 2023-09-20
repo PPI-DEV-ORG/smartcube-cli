bd718a4433e8386d7e4e0449f9dd5df33179983e
osenbal <iqbalmaulana172703@gmail.com>
2023-09-20 22:35:05 +0700

    enhance : send notification to api

--- app.py
+++ app.py
@@ -4,9 +4,14 @@ from internal.modules.USBCamera import *
 from internal.modules.VideoProcessor import *
 from internal.modules.ObjectDetectorModel import *
 import time
+import requests
+import numpy as np
+from concurrent.futures import ThreadPoolExecutor
+
 
 lastDetectionTime = time.time()
 breakTimeWhenObjectDetected = 30 # seconds
+BASE_API_URL = "http://localhost:3000"
 
 def detectObject(camera: CameraDevice, videoProcessor: VideoProcessor):
     model = ObjectDetectorModel(videoProcessor, 0.5, 0.5, 60, 50, handleOnObjectDetected)
@@ -21,8 +26,39 @@ def handleOnObjectDetected(classLabel, confidence, frame):
         print("frame : ", frame)
         print(f"Deteksi objek: {classLabel} dengan kepercayaan {confidence}%")
 
+        # send notification
+        sendNotification(frame)
+
         # Perbarui waktu terakhir callback dipanggil
         lastDetectionTime = currentTime
+
+def sendNotification(frame: np.ndarray):
+    # Membuat ThreadPoolExecutor dengan jumlah thread sesuai kebutuhan
+    # Misalnya, kita gunakan 5 thread di sini
+    with ThreadPoolExecutor(max_workers=2) as executor:
+        # Membuat beberapa tugas untuk mengirim notifikasi
+        executor.submit(handleSendNotification, frame)
+
+def handleSendNotification(frame: np.ndarray):
+    print("Sending notification...")
+
+    data = {
+        "title": "Objek terdeteksi",
+        "description": "Objek terdeteksi pada kamera 1",
+    }
+
+    files = {
+        'image': ('gambar.png', open('1kb.png', 'rb'), 'image/png')
+    }
+
+    response = requests.post(BASE_API_URL + "/notification", data=data, files=files)
+
+    # Cek status kode HTTP untuk memastikan permintaan berhasil
+    if response.status_code == 200:
+        print("Notification sent successfully")
+    else:
+        print(f"Failed to send notification. Status code: {response.status_code}")
+
 
 def main():
 
